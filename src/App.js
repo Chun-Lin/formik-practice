0016c624c47d2d79eb27b0f774dd8ac882f5808d
Chun-Lin <wulin40063@gmail.com>
2018-05-14 17:25:06 +0800

    feat: add validationSchema to check each input format is valid or note

--- src/App.js
+++ src/App.js
@@ -4,13 +4,25 @@ import './App.css'
 import { withFormik, Form, Field } from 'formik'
 import Yup from 'yup'
 
-const App = ({ values, handleChange, handleSubmit }) => {
+const App = ({ values, errors, touched }) => {
+  console.log(touched)
+  console.log(errors)
   return (
-    <Form onSubmit={handleSubmit}>
-      <Field type="email" name="email" placeholder="Email" />
-      <Field type="password" name="password" placeholder="password" />
+    <Form>
+      <div>
+        <Field type="email" name="email" placeholder="Email" />
+        {touched.email &&
+          errors.email && <div style={{ color: 'red' }}>{errors.email}</div>}
+      </div>
+      <div>
+        <Field type="password" name="password" placeholder="password" />
+        {touched.password &&
+          errors.password && (
+            <div style={{ color: 'red' }}>{errors.password}</div>
+          )}
+      </div>
       <label>
-        <Field type="checkbox" name="newsletter" checked={values.newsletter}/>
+        <Field type="checkbox" name="newsletter" checked={values.newsletter} />
         Join our newsletter
       </label>
       <Field component="select" name="plan">
@@ -28,9 +40,17 @@ const FormikApp = withFormik({
       email: email || '',
       password: password || '',
       newsletter: newsletter || false,
-      plan: plan || 'free'
+      plan: plan || 'free',
     }
   },
+  validationSchema: Yup.object().shape({
+    email: Yup.string()
+      .email('Email not valid')
+      .required('Email is required'),
+    password: Yup.string()
+      .min(9, 'Password must be 9 characters or longer')
+      .required('password is required'),
+  }),
   handleSubmit(values) {
     console.log(values)
   },
