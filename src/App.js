35fbe51ae537a4add73fe6f3888a9cb3233f196b
Chun-Lin <wulin40063@gmail.com>
2018-05-14 15:53:24 +0800

    feat: add checkbox and select <Field>

--- src/App.js
+++ src/App.js
@@ -9,16 +9,26 @@ const App = ({ values, handleChange, handleSubmit }) => {
     <Form onSubmit={handleSubmit}>
       <Field type="email" name="email" placeholder="Email" />
       <Field type="password" name="password" placeholder="password" />
+      <label>
+        <Field type="checkbox" name="newsletter" checked={values.newsletter}/>
+        Join our newsletter
+      </label>
+      <Field component="select" name="plan">
+        <option value="free">Free</option>
+        <option value="premium">Premium</option>
+      </Field>
       <button>Submit</button>
     </Form>
   )
 }
 
 const FormikApp = withFormik({
-  mapPropsToValues({ email, password }) {
+  mapPropsToValues({ email, password, newsletter, plan }) {
     return {
       email: email || '',
       password: password || '',
+      newsletter: newsletter || false,
+      plan: plan || 'free'
     }
   },
   handleSubmit(values) {
